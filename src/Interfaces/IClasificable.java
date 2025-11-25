35c117aa7902627ed675e9503d3e770dda08b925
ROMINAP-28 <rominanadielka2006@gmail.com>
2025-11-24 20:53:49 -0500

    24/11

---
+++ src/Interfaces/IClasificable.java
@@ -0,0 +1,12 @@
+
+package Interfaces;
+
+import java.util.List;
+
+
+public interface IClasificable {
+    
+    public void agregarEtiqueta(String tag);
+    
+    public List<String> ObtenerEtiquetas();
+}
---
+++ src/Interfaces/IExportableXML.java
@@ -0,0 +1,11 @@
+
+package Interfaces;
+
+
+public interface IExportableXML {
+    
+    public String serializarXML();
+    
+    public void deserializarXML(String xml);
+
+}
