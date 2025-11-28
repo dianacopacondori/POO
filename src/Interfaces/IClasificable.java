10d33618cd5c36d59878a5827b6c3a0b625f817e
ROMINAP-28 <rominanadielka2006@gmail.com>
2025-11-28 17:36:35 -0500

    comentarios

--- src/Interfaces/IClasificable.java
+++ src/Interfaces/IClasificable.java
@@ -3,10 +3,13 @@ package Interfaces;
 
 import java.util.List;
 
+//@auto Romina
 
 public interface IClasificable {
     
     public void agregarEtiqueta(String idDocumento,String tag);
+    // Metodo para agregar etiquetas, segun codigo (idDocumento)
     
     public List<String> ObtenerEtiquetas(String idDocumento);
+    // metodo para obtener las etiquetas, segun el idDocumento,
 }
